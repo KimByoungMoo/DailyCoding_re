314d977448003e06f1f5c6ee547b5eeb8b01c6e6
KBM <kimbm1114@gmail.com>
2022-12-12 23:59:52 +0900

    test

---
+++ Main1247.java
@@ -0,0 +1,32 @@
+package src;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main1247 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 3; i++) {
+            BigInteger sum = new BigInteger("0");
+            int N = Integer.parseInt(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                BigInteger num1 = new BigInteger(br.readLine());
+                sum = sum.add(num1);
+            }
+
+            if (sum.compareTo(BigInteger.ZERO) == -1) {
+                System.out.println("-");
+            } else if (sum.compareTo(BigInteger.ZERO) ==1) {
+                System.out.println("+");
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
+
---
+++ Main1267.java
@@ -0,0 +1,30 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main1267 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int Y = 0;
+        int M = 0;
+        for (int i = 0; i < N; i++) {
+            int T = Integer.parseInt(br.readLine());
+            Y += ((T/30)+1) * 10;
+            M += ((T/60)+1) * 15;
+        }
+
+        if (Y == M) {
+            System.out.println("Y M " + Y);
+        } else if (Y > M) {
+            System.out.println("M " + M);
+        } else if (Y < M) {
+            System.out.println("Y " + Y);
+        }
+
+    }
+}
