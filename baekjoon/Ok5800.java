e5404d3b2cc3aaf21e39e955e0d5acac899ce4a5
KBM <kimbm1114@gmail.com>
2023-01-08 11:04:48 +0900

    test

---
+++ baekjoon/Ok5800.java
@@ -0,0 +1,36 @@
+package src.baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ok5800 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int K = input.nextInt();
+
+        int min = 0;
+        int max = 0;
+        for (int i = 0; i < K; i++) {
+            int N = input.nextInt();
+            int[] arr = new int[N];
+            for (int j = 0; j < N; j++) {
+                int score = input.nextInt();
+                arr[j] = score;
+            }
+            Arrays.sort(arr);
+            int gap = 0;
+            int largeGap = 0;
+            for (int k = 1; k < arr.length; k++) {
+                gap = Math.abs(arr[k-1] - arr[k]);
+                if (gap > largeGap) {
+                    largeGap = gap;
+                }
+            }
+            min = arr[0];
+            max = arr[arr.length - 1];
+            System.out.println("Class " + (i+1));
+            System.out.println("Max " + max +"," + " Min " + min + "," + " Largest gap " + largeGap);
+        }
+    }
+}
